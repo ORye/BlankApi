736a99af1d7dea34acbba4ac3912edbe2c2da849
Sean O'Reilly <sean@real.vision>
2018-10-07 18:31:37 -0400

    Blank core web api with start of sqlite config

---
+++ ShellApi/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShellApi.Data;
+using System;
+
+namespace ShellApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        private IHostingEnvironment CurrentEnvironment { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            Action<DbContextOptionsBuilder> dbContextOptions;
+            if (CurrentEnvironment.IsDevelopment())
+            {
+                string connectionString = "Data Source=./local.db";
+                dbContextOptions = options => options.UseSqlite(connectionString);
+            }
+            else
+            {
+                // TODO: set up other environment db contexts
+                throw new NotImplementedException("Non development DB providers are not yet set up.");
+            }
+
+            services.AddDbContext<ShellApiContext>(dbContextOptions);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            CurrentEnvironment = env;
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
